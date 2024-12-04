3315b1d03f64113833e84ad04796448c4e8446aa
stasfr <sfworking@yandex.ru>
2024-12-04 09:11:59 +0300

    feat: activationLink create, start mail service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,14 +1,16 @@
-import crypto from 'crypto';
-import prisma from '../../../prisma/client';
+import crypto from "crypto";
+import prisma from "../../../prisma/client";
 
-import type { UserCreate, UserResponse } from '../../types/Auth';
+import mailService from "../mail/mail.service";
+
+import type { UserCreate, UserResponse } from "../../types/Auth";
 
 class AuthService {
   async register(registerPayload: UserCreate): Promise<UserResponse> {
     const { password, email, ...rest } = registerPayload;
 
-    if (!email) throw new Error('Email not provided');
-    if (!password) throw new Error('Password not provided');
+    if (!email) throw new Error("Email not provided");
+    if (!password) throw new Error("Password not provided");
 
     // basic validation
     const isExist = await prisma.user.findUnique({
@@ -17,17 +19,36 @@ class AuthService {
       },
     });
 
-    if (isExist) throw new Error('User already exist');
+    if (isExist) throw new Error("User already exist");
 
     // hashing password
-    const salt = crypto.randomBytes(16).toString('hex');
+    const salt = crypto.randomBytes(16).toString("hex");
     const hash = crypto
-      .pbkdf2Sync(password, salt, 1000, 64, 'sha512')
-      .toString('hex');
+      .pbkdf2Sync(password, salt, 1000, 64, "sha512")
+      .toString("hex");
 
     const user = await prisma.user.create({
-      data: { ...rest, email, salt, password: hash },
+      data: {
+        ...rest,
+        email,
+        salt,
+        password: hash,
+        activation: {
+          create: {
+            activationLink: crypto.randomUUID(),
+          },
+        },
+      },
+      include: { activation: true },
     });
+
+    if (!user) throw new Error("User not created");
+    if (!user.activation) throw new Error("Activation not created");
+
+    await mailService.sendActivationMail(
+      user.email,
+      user.activation.activationLink
+    );
 
     return {
       id: user.id,
---
+++ src/modules/mail/mail.service.ts
@@ -0,0 +1,7 @@
+class MailService {
+  async sendActivationMail(to: string, link: string): Promise<void> {
+    // TODO
+  }
+}
+
+export default new MailService();
